c329433a5729d13670eeba30601f7e160a1f916f
team-cores <sanket.deshpande@coreviwsystems.com>
2024-01-13 13:34:42 +0530

    Added intial commit

---
+++ src/main/java/com/sanketdd/tinderex/TinderExApplication.java
@@ -0,0 +1,13 @@
+package com.sanketdd.tinderex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderExApplication.class, args);
+	}
+
+}
